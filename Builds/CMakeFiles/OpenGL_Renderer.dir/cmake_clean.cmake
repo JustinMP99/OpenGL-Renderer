1677e0a5a7cb0d370447619faaa0749272026e4d
Justin Philie <115652134+JustinMP99@users.noreply.github.com>
2024-08-02 14:12:48 -0400

    Project Setup
    - Added CMake files
    - Created starting folders
    - Verified successful build on Linux

---
+++ Builds/CMakeFiles/OpenGL_Renderer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGL_Renderer.dir/main.cpp.o"
+  "CMakeFiles/OpenGL_Renderer.dir/main.cpp.o.d"
+  "OpenGL_Renderer"
+  "OpenGL_Renderer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGL_Renderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Builds/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/justinp/Desktop/Projects/Graphics/OpenGL-Renderer
+# Build directory: /home/justinp/Desktop/Projects/Graphics/OpenGL-Renderer/Builds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
